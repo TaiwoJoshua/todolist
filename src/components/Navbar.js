cd8d4f2d6a27d7c287d52c0679450de97f1f09e5
[Joshua Taiwo] <[joshuataiwo07@gmail.com]>
2023-08-17 02:55:42 +0100

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar(props){
     return(
         <section className="navbar">
-            <h1>My To-Do Lists</h1>
+            <h1><i className="fas fa-list-check"></i> My To-Do Lists</h1>
             {props.todos.length > 0 && <button className="addtodo-btn addicon-btn" onClick={props.addNewTodo}><i className="fas fa-plus"></i></button>}
         </section>
     );
