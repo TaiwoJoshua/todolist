7b0f88b9aa93d827578ac4de08e2d7f6e557b89e
[Joshua Taiwo] <[joshuataiwo07@gmail.com]>
2023-08-17 03:04:34 +0100

    first commit

--- src/components/Todolists.js
+++ src/components/Todolists.js
@@ -22,10 +22,14 @@ export default function Todolists(props){
         <section className="todolists-section">
             <input type="text" name="todo-title" maxLength={50} onChange={(event) => props.titleChange(event, props.data.id)} value={props.data.title} placeholder="To-Do List Title" />
             <div className="todolists-add">
-                <button className="addicon-btn" onClick={props.toggleShow}><i className={!props.showAdd ? "fas fa-plus" : "fas fa-times"}></i></button>
-                <span>{props.showAdd ? "Close" : "Add"} New To-Do Item</span>
-                {props.showAdd && <input type="text" className="add-todo-input" onChange={props.newTodoChange} placeholder="Your New To-Do Item" value={props.newTodoItem} />}
-                {props.showAdd && <button className="btn" onClick={() => props.addNewTodoItem(props.data.id)}>Add</button>}
+                <span>
+                    <button className="addicon-btn" onClick={props.toggleShow}><i className={!props.showAdd ? "fas fa-plus" : "fas fa-times"}></i></button>
+                    <span>{props.showAdd ? "Close" : "Add"} New To-Do Item</span>
+                </span>
+                <span>
+                    {props.showAdd && <input type="text" className="add-todo-input" onChange={props.newTodoChange} placeholder="Your New To-Do Item" value={props.newTodoItem} />}
+                    {props.showAdd && <button className="btn" onClick={() => props.addNewTodoItem(props.data.id)}>Add</button>}
+                </span>
             </div>
             {props.data.todos.length > 0 && <p className="todolists-status">{completed} / {props.data.todos.length} Completed To-Do Item{props.data.todos.length > 1 ? "s" : ""}</p>}
             <div className="todolists-wrapper">
